a320fe62cd6d2d26499531bd81faef160fd331ae
m-ahmadzafar <161350207+m-ahmadzafar@users.noreply.github.com>
2024-08-05 14:09:51 +0500

    Learned about async await and template literals

--- async.js
+++ async.js
@@ -1,1 +1,13 @@
 // learn about async await fetch and promises
+
+// In our code, we want to ensure that it isnt 
+// accessing an object before it exists. 
+// For Example:
+
+const fetchData = async () => {
+    const data = await fetch("https::/randomapi/api");
+    const name = data.person?.name;
+}
+
+// The ? after person ensures that it is true
+// and the value is only assigned if it is.
